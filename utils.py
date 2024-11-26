3987eaf1b6108b6f8368a0eb58004459b65d1e83
StephMcCallum <stephaniemccallu@u.boisestate.edu>
2024-11-26 13:46:09 -0700

    starting utils file

--- utils.py
+++ utils.py
@@ -3,13 +3,20 @@ import signac
 import hoomd
 import gsd.hoomd
 
-def relabel_center_beads(job, start=0, stop=None, stride=1):
+def relabel_center_beads(job, beads_from_center=2, start=0, stop=None, stride=1):
     """
     Function takes CG signac job as input and creates a new GSD file with copied trajectory and relabels center 5 beads as new type "B".
+
+    Parameters
+    ----------
+
+    job : signac job
+
+    beads_from_center : int, default 2
     
     """
     gsd_file = job.fn("production.gsd")
-    mid_chunk = np.array(range(job.doc.lengths//2 - 2, job.doc.lengths//2 + 3))
+    mid_chunk = np.array(range(job.doc.lengths//2 - beads_from_center, job.doc.lengths//2 + (beads_from_center+1)))
     all_chain_mid_indices = np.array([mid_chunk + job.doc.lengths * i for i in range(job.doc.num_mols)])
 
     original_traj = gsd.hoomd.open(gsd_file)
